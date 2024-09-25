e3e51c7188a1c1b56f7920275d9b844b12ae7252
Charlie Lamb <charlielamb20@icloud.com>
2024-09-25 22:42:27 +0100

    added path as param

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use std::net::SocketAddr;
 use axum::response::{Html, IntoResponse};
 use axum::{Router, ServiceExt};
-use axum::extract::Query;
+use axum::extract::{Path, Query};
 use axum::routing::get;
 use axum_server::Server;
 use serde::Deserialize;
@@ -11,7 +11,7 @@ use serde::Deserialize;
 #[tokio::main]
 async fn main() {
     let routes_hello = Router::new().route("/hello",
-    get(handler_hello));
+    get(handler_hello)).route("/hello2/:name",get(hello_handler2));
 
     let addr = SocketAddr::from(([127,0,0,1],8080));
     println!("->> Listening on {addr}\n");
@@ -30,6 +30,13 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}","HANDLER");
 
     let name = params.name.as_deref().unwrap_or("world");
+
+    Html(format!("Hello {name}..."))
+}
+
+async fn hello_handler2(Path(name): Path<String>) -> impl IntoResponse {
+
+    println!("->> {:<12} - handler_hello - {name:?}","HANDLER");
 
     Html(format!("Hello {name}..."))
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -5,6 +5,6 @@ use anyhow::Result;
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080");
-    hc?.do_get("/hello?name=hi").await?.print().await?;
+    hc?.do_get("/hello2/Charlie").await?.print().await?;
     Ok(())
 }
